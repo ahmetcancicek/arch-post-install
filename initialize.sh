80ff64f02e786cf385f8a948f30b371ed8dcb8f6
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2023-07-01 16:32:57 +0300

    Add installation

--- initialize.sh
+++ initialize.sh
@@ -1,8 +1,46 @@
 #!/bin/sh
 
 pacman -Syu --noconfirm
+
+
+# Bluetooth
+systemctl start bluetooth
+systemctl enable bluetooth
+
+
+# yay
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg si
+
+
+# GNOME Extensions and Browser Connector
+pacman -S gnome-shell-extensions
+pacman -S gnome-browser-connector
+
+
 pacman -S --noconfirm tree \
 	curl \
 	wget \
+	tree \
 
-pacman -S vim
+
+pacman -S --noconfirm vim
+
+
+pacman -S --noconfirm gnome-boxes
+
+pacman -S --noconfirm terminator
+
+
+yay -S visual-studio-code-bin
+
+pacman -S git
+
+pacman -S zsh
+
+pacman -S spotify-launcher
+
+
+yay -S google-chrome
+yay -S keepassxc
