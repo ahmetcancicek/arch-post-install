01b0162224d4384aa957ea99300553af60130c00
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-02-17 09:27:02 +0300

    Add new features to installation
    Installation:
    * Opera
    * Edge
    * Brave
    * Discord
    * Web Apps
    * pCloud
    * Timeshift
    * Gaparted
    * Kdenlive
    * Krita
    * Inkscape
    * Raindrop
    * Go
    * Anki
    
    Update:
    * IntellijIDEA
    * DataGrip

--- main.sh
+++ main.sh
@@ -20,13 +20,15 @@ EOL
 fi
 
 # Set Version
-JETBRAINS_VERSION=2023.3.2
-POSTMAN_VERSION=10.17
+JETBRAINS_VERSION=2023.3.4
+GO_VERSION=1.22.0
+POSTMAN_VERSION=10.23
 MAVEN=3
-MAVEN_VERSION=3.9.4
+MAVEN_VERSION=3.9.6
 GRADLE_VERSION=8.2.1
-SPRING=3.0.0
-SPRING_VERSION=3.1.2
+SPRING_VERSION=3.2.2
+ANKI_VERSION=23.12.1
+DROIDCAM_VERSION=2.1.2
 
 # Get USER name
 USER=$(logname)
@@ -37,15 +39,37 @@ HOME=/home/$USER
 # Go TEMP folder
 cd /tmp
 
-
+# Update
+printf "\n${BLUE}========================Installing Updating========================${ENDCOLOR}\n"
 pacman -Syu --noconfirm
+printf "${GREEN}========================Updated successfully!========================${ENDCOLOR}\n"
+
 
 # Install Essentials
-pacman -S --noconfirm tree \
-	curl \
-	wget \
-	tree \
+declare -A essential
+essentials=(
+  tree
+	curl
+	wget
+	htop
   usbutils
+  base-devel
+  ntfsprogs
+  dosfstools
+)
+printf "\n${BLUE}========================Installing standard package $1========================${ENDCOLOR}\n"
+for key in "${essentials[@]}"; do
+  pacman -S $key
+done
+printf "\n${BLUE}===============Standard packages are installed successfully=============== ${ENDCOLOR}\n"
+
+
+# Go /tmp
+go_temp() {
+  cd /tmp
+}
+
+
 
 # Install Bluetooth Driver
 pacman -S --noconfirm bluez \
@@ -53,56 +77,6 @@ pacman -S --noconfirm bluez \
 	blueman
 systemctl start bluetooth.service
 systemctl enable bluetooth.service
-
-# GNOME Extensions
-pacman -S --noconfirm gnome-shell-extensions
-pacman -S --noconfirm gnome-browser-connector
-pacman -S --noconfirm gnome-themes-extra
-
-# Install Vim
-pacman -S --noconfirm vim
-
-# Install Gnome-Boxes
-pacman -S --noconfirm gnome-boxes
-
-# Install Terminator
-pacman -S --noconfirm terminator
-
-# Install GIMP
-pacman -S --noconfirm gimp
-
-# Install GIT
-pacman -S --noconfirm  git
-
-# Install Thunderbird
-pacman -S --noconfirm thunderbird
-
-# Install KeePassXC
-pacman -S --noconfirm keepassxc
-
-# Install Gnome Boxes
-pacman -S --noconfirm gnome-boxes
-
-# Install Spotify
-pacman -S --noconfirm spotify-launcher
-
-# Install LibreOffice
-pacman -S --noconfirm libreoffice
-
-# Install OpenVPN
-pacman -S --noconfirm openvpn
-pacman -S --noconfirm networkmanager-openvpn
-
-# Install Zoom
-wget https://zoom.us/client/5.16.2.8828/zoom_x86_64.pkg.tar.xz
-pacman -U zoom_x86_64.pkg.tar.xz
-
-#
-pacman -S libappindicator-gtk3
-
-# Gnome Sound Recorder
-pacman -S gnome-sound-recorder
-
 
 # Install AUR
 cd ~
@@ -114,8 +88,88 @@ makepkg -si
 yay -S snapd
 systemctl start snapd.service
 
+# Install Flatpak Repository
+pacman -S --noconfirm flatpak
+
+# GNOME Extensions
+pacman -S --noconfirm gnome-shell-extensions
+pacman -S --noconfirm gnome-browser-connector
+pacman -S --noconfirm gnome-themes-extra
+
 # Install Google Chrome
 yay -S google-chrome
+
+# Install Chromium
+pacman -S --noconfirm chromium
+
+# Install Spotify
+pacman -S --noconfirm spotify-launcher
+
+# Install Opera
+pacman -S --noconfirm opera
+
+# Install Microsoft Edge
+yay -S microsoft-edge-stable
+
+# Install Brave
+yay -S brave-bin
+
+# Install Zoom
+wget https://zoom.us/client/5.16.2.8828/zoom_x86_64.pkg.tar.xz
+pacman -U zoom_x86_64.pkg.tar.xz
+
+# Install Discord
+flatpak install discord
+
+# Install Thunderbird
+pacman -S --noconfirm thunderbird
+
+# Install GIT
+pacman -S --noconfirm  git
+
+# Install Vim
+pacman -S --noconfirm vim
+
+# Install Gnome-Boxes
+pacman -S --noconfirm gnome-boxes
+
+# Install Terminator
+pacman -S --noconfirm terminator
+
+# Install Web Apps
+yay -S webapp-manager
+
+# Install GIMP
+pacman -S --noconfirm gimp
+
+# Install Dropbox
+yay -S dropbox
+
+# Install pCloud
+yay -S pcloud-drive
+
+# Install KeePassXC
+pacman -S --noconfirm keepassxc
+
+# Install Gnome Boxes
+pacman -S --noconfirm gnome-boxes
+
+# Install VirtualBox
+pacman -S --noconfirm virtualbox-host-modules-arch
+pacman -S virtualbox
+modprobe vboxdrv
+
+# Install LibreOffice
+pacman -S --noconfirm libreoffice
+
+# Install OpenVPN
+pacman -S --noconfirm openvpn
+pacman -S --noconfirm networkmanager-openvpn
+
+pacman -S libappindicator-gtk3
+
+# Gnome Sound Recorder
+pacman -S gnome-sound-recorder
 
 # Install Galaxy Buds Client
 yay -S galaxybudsclient-bin
@@ -125,6 +179,31 @@ yay -S visual-studio-code-bin
 
 # Android Tools
 yay -S android-tools
+
+# Install Timeshift
+pacman -S --noconfirm timeshift
+
+# Install Gparted
+pacman -S --noconfirm gparted
+
+# Install Kdenlive
+pacman -S --noconfirm kdenlive
+
+# Install Krita
+pacman -S --noconfirm krita
+
+# Install Inkscape
+pacman -S --noconfirm inkscape
+
+# Install Raindrop
+snap install raindrop
+
+# Install Anki
+pacman -S --noconfirm zstd
+wget https://github.com/ankitects/anki/releases/download/23.12.1/anki-${ANKI_VERSION}-linux-qt6.tar.zst -O anki.tar.zst
+tar xaf anki.tar.zst
+cd anki-${ANKI_VERSION}-linux-qt6
+sudo ./install.sh
 
 # Install JAVA
 pacman -S --noconfirm jdk-openjdk
@@ -140,7 +219,6 @@ archlinux-java set jdk-21.0.1
 
 wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
 tar xf jdk-17_linux-x64_bin.tar.gz -C /usr/lib/jvm
-
 
 # Install Sprint CLI
 wget https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-cli/${SPRING_VERSION}/spring-boot-cli-${SPRING_VERSION}-bin.tar.gz
@@ -164,7 +242,6 @@ echo 'export JAVA_HOME=/usr/lib/jvm/jdk-20.0.2/' >> /etc/profile.d/maven.sh
 chmod +x /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
 
-
 # Install Gradle
 rm -rf gradle-${GRADLE_VERSION}-bin.zip
 wget https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip
@@ -175,7 +252,6 @@ echo -ne 'export PATH=$PATH:/opt/gradle/bin' >> /etc/profile.d/gradle.sh
 chmod +x /etc/profile.d/gradle.sh
 source /etc/profile.d/gradle.sh
 
-
 # Install IntelliJ IDEA
 wget https://download.jetbrains.com/idea/ideaIU-${JETBRAINS_VERSION}.tar.gz -O ideaIU.tar.gz
 tar -xf ideaIU.tar.gz -C /opt
@@ -183,28 +259,53 @@ mv /opt/idea-IU-* /opt/idea-IU-${JETBRAINS_VERSION}
 ln -s /opt/idea-IU-${JETBRAINS_VERSION} /opt/idea
 ln -s /opt/idea/bin/idea.sh /usr/local/bin/idea
 echo "[Desktop Entry]
-          Version=1.0
-          Type=Application
-          Name=IntelliJ IDEA Ultimate Edition
-          Icon=/opt/idea/bin/idea.svg
-          Exec=/opt/idea/bin/idea.sh %f
-          Comment=Capable and Ergonomic IDE for JVM
-          Categories=Development;IDE;
-          Terminal=false
-          StartupWMClass=jetbrains-idea
-          StartupNotify=true;" >>/usr/share/applications/jetbrains-idea.desktop
+      Version=1.0
+      Type=Application
+      Name=IntelliJ IDEA Ultimate Edition
+      Icon=/opt/idea/bin/idea.svg
+      Exec=/opt/idea/bin/idea.sh %f
+      Comment=Capable and Ergonomic IDE for JVM
+      Categories=Development;IDE;
+      Terminal=false
+      StartupWMClass=jetbrains-idea
+      StartupNotify=true;" >>/usr/share/applications/jetbrains-idea.desktop
+
+# Install DataGrip
+wget https://download.jetbrains.com/datagrip/datagrip-${JETBRAINS_VERSION}.tar.gz
+tar -xzf datagrip-${JETBRAINS_VERSION}.tar.gz -C /opt
+mv /opt/DataGrip-* /opt/DataGrip-${JETBRAINS_VERSION}
+ln -s /opt/DataGrip-${JETBRAINS_VERSION} /opt/datagrip
+ln -s /opt/datagrip/bin/datagrip.sh /usr/local/bin/datagrip
+echo "[Desktop Entry]
+      Version=1.0
+      Type=Application
+      Name=DataGrip
+      Icon=/opt/datagrip/bin/datagrip.png
+      Exec=/opt/datagrip/bin/datagrip.sh
+      Terminal=false
+      Categories=Development;IDE;" >>/usr/share/applications/jetbrains-datagrip.desktop
 
 # Install Postman
- curl https://dl.pstmn.io/download/latest/linux64 --output postman-${POSTMAN_VERSION}-linux-x64.tar.gz
- tar -xzf postman-${POSTMAN_VERSION}-linux-x64.tar.gz -C /opt
- echo "[Desktop Entry]
-          Encoding=UTF-8
-          Name=Postman
-          Exec=/opt/Postman/app/Postman %U
-          Icon=/opt/Postman/app/resources/app/assets/icon.png
-          Terminal=false
-          Type=Application
-          Categories=Development;" >>/usr/share/applications/Postman.desktop
+curl https://dl.pstmn.io/download/latest/linux64 --output postman-${POSTMAN_VERSION}-linux-x64.tar.gz
+tar -xzf postman-${POSTMAN_VERSION}-linux-x64.tar.gz -C /opt
+echo "[Desktop Entry]
+      Encoding=UTF-8
+      Name=Postman
+      Exec=/opt/Postman/app/Postman %U
+      Icon=/opt/Postman/app/resources/app/assets/icon.png
+      Terminal=false
+      Type=Application
+      Categories=Development;" >>/usr/share/applications/Postman.desktop
+
+# Install GoLang
+wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+touch /etc/profile.d/go.sh
+echo -e '\n# GoLang configuration ' >> /etc/profile.d/go.sh
+echo 'export PATH="$PATH:/usr/local/go/bin"' >> /etc/profile.d/go.sh
+echo 'export GOPATH="$HOME/go"' >> /etc/profile.d/go.sh
+chmod +x /etc/profile.d/go.sh
+source /etc/profile.d/go.sh
 
 # Install Docker
 pacman -S --noconfirm docker
@@ -215,9 +316,6 @@ systemctl enable docker.service
 usermod -aG docker $USER
 pacman -S --noconfirm docker-compose
 
-# Install Flatpak Repository
-pacman -S --noconfirm flatpak
-
 # Install Droidcam
 wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_2.1.1.zip
 unzip droidcam_latest.zip -d droidcam
@@ -226,11 +324,9 @@ cd ..
 pacman -S --noconfirm android-tools v4l2loopback-dkms ffmpeg android-udev --needed
 pacman -S --noconfirm linux-headers
 
-
 # Install Nvidia Driver
 # pacman -S nvidia \
 # nvidia-settings
-
 
 # Install ZSH & Oh My ZSH
 pacman -S --noconfirm zsh
