10b49d10e77b770c4c1905778fee18a3ca5ffbdf
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-02-17 12:10:15 +0300

    Refactor the bash script

--- main.sh
+++ main.sh
@@ -70,147 +70,239 @@ go_temp() {
 }
 
 
+# Installation Message
+print_installation_message() {
+  printf "\n${BLUE}===============================Installing $1==============================${ENDCOLOR}\n"
+}
+
+# Installation Success Message
+print_installation_message_success() {
+  printf "${GREEN}========================$1 is installed successfully!========================${ENDCOLOR}\n"
+  go_temp
+}
+
 
 # Install Bluetooth Driver
+print_installation_message Bluetooth
 pacman -S --noconfirm bluez \
 	bluez-utils \
 	blueman
 systemctl start bluetooth.service
 systemctl enable bluetooth.service
+print_installation_message_success Bluetooth
 
 # Install AUR
+print_installation_message AUR
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+print_installation_message_success AUR
 
 # Insall Snap
+print_installation_message Snap
 yay -S snapd
 systemctl enable --now snapd.socket
 ln -s /var/lib/snapd/snap /snap
 systemctl start snapd.service
+print_installation_message_success Snap
 
+print_installation_message Flatpak
 # Install Flatpak Repository
 pacman -S --noconfirm flatpak
+print_installation_message_success Flatpak
 
 # GNOME Extensions
+print_installation_message Extensions
 pacman -S --noconfirm gnome-shell-extensions
 pacman -S --noconfirm gnome-browser-connector
 pacman -S --noconfirm gnome-themes-extra
+print_installation_message_success Extensions
 
-# Install Chromium
+# Install Firefox
+print_installation_message Firefox
 pacman -S --noconfirm firefox
+print_installation_message_success Firefox
 
 # Install Google Chrome
+print_installation_message Chrome
 yay -S google-chrome
+print_installation_message_success Chrome
 
 # Install Chromium
+print_installation_message Chromium
 pacman -S --noconfirm chromium
+print_installation_message_success Chromium
 
 # Install Spotify
+print_installation_message Spotify
 pacman -S --noconfirm spotify-launcher
+print_installation_message_success Spotify
 
 # Install Opera
+print_installation_message Opera
 pacman -S --noconfirm opera
+print_installation_message_success Opera
 
 # Install Microsoft Edge
+print_installation_message Edge
 yay -S microsoft-edge-stable
+print_installation_message_success Edge
 
 # Install Brave
+print_installation_message Brave
 yay -S brave-bin
+print_installation_message_success Brave
 
 # Install Zoom
+print_installation_message Zoom
 wget https://zoom.us/client/5.16.2.8828/zoom_x86_64.pkg.tar.xz
 pacman -U --noconfirm zoom_x86_64.pkg.tar.xz
+print_installation_message_success Zoom
 
 # Install Discord
+print_installation_message Discord
 pacman -S discord
+print_installation_message_success Discord
 
 # Install Thunderbird
+print_installation_message Thunderbird
 pacman -S --noconfirm thunderbird
+print_installation_message_success Thunderbird
 
 # Install GIT
+print_installation_message GIT
 pacman -S --noconfirm  git
+print_installation_message_success GIT
 
 # Install Vim
+print_installation_message Vim
 pacman -S --noconfirm vim
+print_installation_message_success Vim
 
 # Install Gnome-Boxes
+print_installation_message Boxes
 pacman -S --noconfirm gnome-boxes
+print_installation_message_success Boxes
 
 # Install Terminator
+print_installation_message Terminator
 pacman -S --noconfirm terminator
+print_installation_message_success Terminator
 
 # Install Web Apps
+print_installation_message WebApps
 yay -S webapp-manager
+print_installation_message_success WebApps
 
 # Install GIMP
+print_installation_message GIMP
 pacman -S --noconfirm gimp
+print_installation_message_success GIMP
 
 # Install Dropbox
+print_installation_message Dropbox
 yay -S dropbox
+print_installation_message_success Dropbox
 
 # Install pCloud
+print_installation_message pCloud
 yay -S pcloud-drive
+print_installation_message_success pCloud
 
 # Install KeePassXC
+print_installation_message KeePassXC
 pacman -S --noconfirm keepassxc
+print_installation_message_success KeePassXC
 
 # Install Gnome Boxes
+print_installation_message Boxes
 pacman -S --noconfirm gnome-boxes
+print_installation_message_success Boxes
 
 # Install VirtualBox
+print_installation_message VirtualBox
 pacman -S --noconfirm virtualbox-host-modules-arch
 pacman -S --noconfirm virtualbox
 modprobe vboxdrv
+print_installation_message_success VirtualBox
 
 # Install LibreOffice
+print_installation_message LibreOffice
 pacman -S --noconfirm libreoffice
+print_installation_message_success LibreOffice
 
 # Install OpenVPN
+print_installation_message OpenVPN
 pacman -S --noconfirm openvpn
 pacman -S --noconfirm networkmanager-openvpn
+print_installation_message_success OpenVPN
 
-pacman -S libappindicator-gtk3
+print_installation_message libappindicator
+pacman -S --noconfirm libappindicator-gtk3
+print_installation_message_success libappindicator
 
 # Gnome Sound Recorder
-pacman -S gnome-sound-recorder
+print_installation_message SoundRecorder
+pacman -S --noconfirm gnome-sound-recorder
+print_installation_message_success SoundRecorder
 
 # Install Galaxy Buds Client
+print_installation_message galaxybudsclient
 yay -S galaxybudsclient-bin
+print_installation_message_success galaxybudsclient
 
 # Install VSCODE
+print_installation_message VSCODE
 yay -S visual-studio-code-bin
+print_installation_message_success VSCODE
 
 # Android Tools
+print_installation_message AndroidTools
 yay -S android-tools
+print_installation_message_success AndroidTools
 
 # Install Timeshift
+print_installation_message Timeshift
 pacman -S --noconfirm timeshift
+print_installation_message_success Timeshift
 
 # Install Gparted
+print_installation_message Gparted
 pacman -S --noconfirm gparted
+print_installation_message_success Gparted
 
 # Install Kdenlive
+print_installation_message Kdenlive
 pacman -S --noconfirm kdenlive
+print_installation_message_success Kdenlive
 
 # Install Krita
+print_installation_message Krita
 pacman -S --noconfirm krita
+print_installation_message_success Krita
 
 # Install Inkscape
+print_installation_message Inkscape
 pacman -S --noconfirm inkscape
+print_installation_message_success Inkscape
 
 # Install Raindrop
+print_installation_message Raindrop
 snap install raindrop
+print_installation_message_success Raindrop
 
 # Install Anki
+print_installation_message Anki
 pacman -S --noconfirm zstd
 wget https://github.com/ankitects/anki/releases/download/23.12.1/anki-${ANKI_VERSION}-linux-qt6.tar.zst -O anki.tar.zst
 tar xaf anki.tar.zst
 cd anki-${ANKI_VERSION}-linux-qt6
 sudo ./install.sh
+print_installation_message_success Anki
 
 # Install JAVA
+print_installation_message JAVA
 pacman -S --noconfirm jdk-openjdk
 archlinux-java status
 wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
@@ -224,8 +316,10 @@ archlinux-java set jdk-21.0.1
 
 wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
 tar xf jdk-17_linux-x64_bin.tar.gz -C /usr/lib/jvm
+print_installation_message_success JAVA
 
 # Install Sprint CLI
+print_installation_message SprintCLI
 wget https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-cli/${SPRING_VERSION}/spring-boot-cli-${SPRING_VERSION}-bin.tar.gz
 tar xf spring-boot-cli-${SPRING_VERSION}-bin.tar.gz -C /opt
 echo -e "\n# Spring Boot CLI" >> /etc/profile.d/spring.sh
@@ -234,8 +328,10 @@ echo "${SPRING_VERSION}" >> /etc/profile.d/spring.sh
 echo 'export PATH=$PATH:$HOME/bin:$SPRING_HOME/bin' >> /etc/profile.d/spring.sh
 chmod +x /etc/profile.d/spring.sh
 source /etc/profile.d/spring.sh
+print_installation_message_success SprintCLI
 
 # Install Maven
+print_installation_message Maven
 wget https://dlcdn.apache.org/maven/maven-${MAVEN}/${MAVEN_VERSION}/binaries/apache-maven-3.9.4-bin.tar.gz
 tar xzf apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt
 ln -s /opt/apache-maven-${MAVEN_VERSION} /opt/maven
@@ -246,8 +342,10 @@ echo 'export PATH=${M2_HOME}/bin:${PATH}' >> /etc/profile.d/maven.sh
 echo 'export JAVA_HOME=/usr/lib/jvm/jdk-20.0.2/' >> /etc/profile.d/maven.sh
 chmod +x /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
+print_installation_message_success Maven
 
 # Install Gradle
+print_installation_message Gradle
 rm -rf gradle-${GRADLE_VERSION}-bin.zip
 wget https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip
 unzip -d /opt/ gradle-${GRADLE_VERSION}-bin.zip
@@ -256,8 +354,10 @@ echo -e '\n# Gradle Configuration' >> /etc/profile.d/gradle.sh
 echo -ne 'export PATH=$PATH:/opt/gradle/bin' >> /etc/profile.d/gradle.sh
 chmod +x /etc/profile.d/gradle.sh
 source /etc/profile.d/gradle.sh
+print_installation_message_success Gradle
 
 # Install IntelliJ IDEA
+print_installation_message IntelliJIDEA
 wget https://download.jetbrains.com/idea/ideaIU-${JETBRAINS_VERSION}.tar.gz -O ideaIU.tar.gz
 tar -xf ideaIU.tar.gz -C /opt
 mv /opt/idea-IU-* /opt/idea-IU-${JETBRAINS_VERSION}
@@ -274,8 +374,10 @@ echo "[Desktop Entry]
       Terminal=false
       StartupWMClass=jetbrains-idea
       StartupNotify=true;" >>/usr/share/applications/jetbrains-idea.desktop
+print_installation_message_success IntelliJIDEA
 
 # Install DataGrip
+print_installation_message DataGrip
 wget https://download.jetbrains.com/datagrip/datagrip-${JETBRAINS_VERSION}.tar.gz
 tar -xzf datagrip-${JETBRAINS_VERSION}.tar.gz -C /opt
 mv /opt/DataGrip-* /opt/DataGrip-${JETBRAINS_VERSION}
@@ -289,8 +391,10 @@ echo "[Desktop Entry]
       Exec=/opt/datagrip/bin/datagrip.sh
       Terminal=false
       Categories=Development;IDE;" >>/usr/share/applications/jetbrains-datagrip.desktop
+print_installation_message_success DataGrip
 
 # Install Postman
+print_installation_message Postman
 curl https://dl.pstmn.io/download/latest/linux64 --output postman-${POSTMAN_VERSION}-linux-x64.tar.gz
 tar -xzf postman-${POSTMAN_VERSION}-linux-x64.tar.gz -C /opt
 echo "[Desktop Entry]
@@ -301,8 +405,10 @@ echo "[Desktop Entry]
       Terminal=false
       Type=Application
       Categories=Development;" >>/usr/share/applications/Postman.desktop
+print_installation_message_success Postman
 
 # Install GoLang
+print_installation_message GoLang
 wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
 touch /etc/profile.d/go.sh
@@ -311,8 +417,10 @@ echo 'export PATH="$PATH:/usr/local/go/bin"' >> /etc/profile.d/go.sh
 echo 'export GOPATH="$HOME/go"' >> /etc/profile.d/go.sh
 chmod +x /etc/profile.d/go.sh
 source /etc/profile.d/go.sh
+print_installation_message_success GoLang
 
 # Install Docker
+print_installation_message Docker
 pacman -S --noconfirm docker
 docker version
 systemctl start docker.service
@@ -320,20 +428,24 @@ systemctl status docker.service
 systemctl enable docker.service
 usermod -aG docker $USER
 pacman -S --noconfirm docker-compose
+print_installation_message_success Docker
 
 # Install Droidcam
+print_installation_message Droidcam
 wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_2.1.1.zip
 unzip droidcam_latest.zip -d droidcam
 cd droidcam && ./install-client
 cd ..
 pacman -S --noconfirm android-tools v4l2loopback-dkms ffmpeg android-udev --needed
 pacman -S --noconfirm linux-headers
+print_installation_message_success Droidcam
 
 # Install Nvidia Driver
 # pacman -S nvidia \
 # nvidia-settings
 
 # Install ZSH & Oh My ZSH
+print_installation_message ZSH
 pacman -S --noconfirm zsh
 chsh -s /usr/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -341,3 +453,27 @@ pacman -S --noconfirm powerline-fonts
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+print_installation_message_success ZSH
+
+printf "\n${GREEN}"
+cat <<EOL
+===========================================================================
+Congratulations, everything you wanted to install is installed!
+===========================================================================
+EOL
+printf "${ENDCOLOR}\n"
+
+cat <<EOL
+
+EOL
+
+printf ${RED}
+read -p "Are you going to reboot this machine for stability? (y/n): " -n 1 answer
+if [[ $answer =~ ^[Yy]$ ]]; then
+  reboot
+fi
+printf ${ENDCOLOR}
+
+cat <<EOL
+
+EOL
