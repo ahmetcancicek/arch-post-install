43a7c15a061484a0374c8a273a7d9e047d6b0ee8
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-02-24 23:45:45 +0300

    Refactor the AUR installation

--- aur.sh
+++ aur.sh
@@ -45,7 +45,6 @@ print_installation_message_success() {
   go_temp
 }
 
-
 print_installation_message AUR
 cd $HOME
 rm -rf yay
@@ -54,59 +53,6 @@ cd yay
 makepkg -si --noconfirm
 print_installation_message_success AUR
 
-# Insall Snap
-print_installation_message Snap
-yes | yay -S snapd
-systemctl enable --now snapd.socket
-ln -s /var/lib/snapd/snap /snap
-systemctl start snapd.service
-print_installation_message_success Snap
-
-# Install Google Chrome
-print_installation_message Chrome
-yes |  yay -S google-chrome
-# yay -S google-chrome --answerdiff=None
-print_installation_message_success Chrome
-
-# Install Microsoft Edge
-print_installation_message Edge
-yes | yay -S microsoft-edge-stable
-print_installation_message_success Edge
-
-# Install Brave
-print_installation_message Brave
-yes | yay -S brave-bin
-print_installation_message_success Brave
-
-# Install Web Apps
-print_installation_message WebApps
-yes | yay -S webapp-manager
-print_installation_message_success WebApps
-
-# Install Dropbox
-print_installation_message Dropbox
-yes | yay -S dropbox
-print_installation_message_success Dropbox
-
-# Install pCloud
-print_installation_message pCloud
-yes | yay -S pcloud-drive
-print_installation_message_success pCloud
-
-# Install Galaxy Buds Client
-print_installation_message galaxybudsclient
-yes | yay -S galaxybudsclient-bin
-print_installation_message_success galaxybudsclient
-
-# Install VSCODE
-print_installation_message VSCODE
-yes | yay -S visual-studio-code-bin
-print_installation_message_success VSCODE
-
-# Android Tools
-print_installation_message AndroidTools
-yes | yay -S android-tools
-print_installation_message_success AndroidTools
 
 printf "\n${GREEN}"
 cat <<EOL
