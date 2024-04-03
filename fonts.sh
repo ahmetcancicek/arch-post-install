90face469de9537005dfd10dcc7505cf4dbed614
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-04-03 09:36:13 +0300

    Fix bugs

--- fonts.sh
+++ fonts.sh
@@ -43,8 +43,6 @@ print_installation_message() {
 
 # Installation Success Message
 print_installation_message_success() {
-  cd /usr/local/share/fonts/
-  fc-cache -vf
   printf "${GREEN}========================$1 is installed successfully!========================${ENDCOLOR}\n"
   go_temp
 }
@@ -61,111 +59,151 @@ print_installation_message_success Powerline-Fonts
 
 # Roboto
 print_installation_message Roboto
+rm -rf roboto.zip
 curl -o roboto.zip https://www.fontsquirrel.com/fonts/download/roboto
 unzip roboto.zip -d roboto
 rm -rf /usr/local/share/fonts/roboto
 mv roboto /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Roboto
 
 # Noto Sans
 print_installation_message Noto-Sans
+rm -rf noto-sans.zip
 curl -o noto-sans.zip https://www.fontsquirrel.com/fonts/download/noto-sans
 unzip noto-sans.zip -d noto-sans
 rm -rf /usr/local/share/fonts/noto-sans
 mv noto-sans /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Noto-Sans
 
 # Fira Mono
 print_installation_message Fira-Mono
-curl -o fira-mono.ziphttps://www.fontsquirrel.com/fonts/download/fira-mono
+rm -rf fira-mono.zip
+curl -o fira-mono.zip https://www.fontsquirrel.com/fonts/download/fira-mono
 unzip fira-mono.zip -d fira-mono
 rm -rf /usr/local/share/fonts/fira-mono
 mv fira-mono /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Fira-Mono
 
 # Clear Sans
 print_installation_message Clear-Sans
+rm -rf clear-sans.zip
 curl -o clear-sans.zip https://www.fontsquirrel.com/fonts/download/clear-sans
 unzip clear-sans.zip -d clear-sans
 rm -rf /usr/local/share/fonts/clear-sans
 mv clear-sans /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Clear-Sans
 
 # Fira Sans Fira-Sans
 print_installation_message Fira-Sans
+rm -rf fira-sans.zip
 curl -o fira-sans.zip https://www.fontsquirrel.com/fonts/download/fira-sans
 unzip fira-sans.zip -d fira-sans
-rm -rf /usr/local/share/fonts/fira_sans
+rm -rf /usr/local/share/fonts/fira-sans
 mv fira-sans /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Fira-Sans
 
 # Roboto Slab
 print_installation_message Roboto-Slab
-curl -o roboto-slab.zip https://fonts.google.com/download?family=Roboto%20Slab
+rm -rf roboto-slap.zip
+curl -o roboto-slab.zip https://www.fontsquirrel.com/fonts/download/roboto-slab
 unzip roboto-slab.zip -d roboto-slab
 rm -rf /usr/local/share/fonts/roboto-slab
 mv roboto-slab /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Roboto-Slab
 
 # Overpass
 print_installation_message Overpass
+rm -rf overpass.zip
 curl -o overpass.zip https://www.fontsquirrel.com/fonts/download/overpass
 unzip overpass.zip -d overpass
 rm -rf /usr/local/share/fonts/overpass
 mv overpass /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Overpass
 
 # Ubuntu
 print_installation_message Ubuntu
+rm -rf ubuntu.zip
 curl -o ubuntu.zip https://www.fontsquirrel.com/fonts/download/ubuntu
 unzip ubuntu.zip -d ubuntu
 rm -rf /usr/local/share/fonts/ubuntu
 mv ubuntu /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Ubuntu
 
 # Ubuntu Mono
 print_installation_message Ubuntu-Mono
+rm -rf ubuntu-mono.zip
 curl -o ubuntu-mono.zip https://www.fontsquirrel.com/fonts/download/ubuntu-mono
 unzip ubuntu-mono.zip -d ubuntu-mono
 rm -rf /usr/local/share/fonts/ubuntu-mono
 mv ubuntu-mono /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Ubuntu-Mono
 
 # Overpass Mono
 print_installation_message Overpass-Mono
+rm -rf overpass-mono.zip
 curl -o overpass-mono.zip https://www.fontsquirrel.com/fonts/download/overpass-mono
 unzip overpass-mono.zip -d overpass-mono
 rm -rf /usr/local/share/fonts/overpass-mono
 mv overpass-mono /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Overpass-Mono
 
 # JetBrains Mono Font Family
 print_installation_message JetBrains-Mono
+rm -rf JetBrainsMono-2.242.zip
 rm -rf /usr/local/share/fonts/jetbrains-mono
 wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
-unzip JetBrainsMono-2.242 -d JetBrainsMono-2.242
+unzip JetBrainsMono-2.242.zip -d JetBrainsMono-2.242
 mkdir jetbrains-mono
 mv JetBrainsMono-2.242/fonts/ttf/** jetbrains-mono
-mv jetbrains-mono /usr/local/share/fonts-
+rm -rf JetBrainsMono-2.242
+mv jetbrains-mono /usr/local/share/fonts
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success JetBrains-Mono
 
 # Inter
 print_installation_message Inter
+rm -rf Inter-3.19.zip
 rm -rf /usr/local/share/fonts/Inter-3.19
 wget https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip
 unzip Inter-3.19.zip -d Inter-3.19
 rm -rf /usr/local/share/fonts/inter-3.19
 mv Inter-3.19 /usr/local/share/fonts/
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Inter
 
 # Hack v3
 print_installation_message Hack
+rm -rf hack-v3.zip
 rm -rf /usr/local/share/fonts/hack
 wget -O hack-v3.zip https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
 unzip hack-v3.zip -d hack-v3
 mkdir hack
 mv hack-v3/ttf/* hack
 mv hack /usr/local/share/fonts/hack
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Hack
 
 # Install Monaco
@@ -174,6 +212,8 @@ rm -rf /usr/local/share/fonts/monaco
 mkdir /usr/local/share/fonts/monaco
 cd /usr/local/share/fonts/monaco
 wget http://www.gringod.com/wp-upload/software/Fonts/Monaco_Linux.ttf
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success Monaco
 
 # Install MesloLGS
@@ -185,11 +225,15 @@ wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20R
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+cd /usr/local/share/fonts/
+fc-cache -f
 print_installation_message_success MesloLGS
 
 
 printf "${GREEN}"
 cat <<EOL
+
+
 ===============================================================
 Congratulations, everything you wanted to install is installed!
 ===============================================================
