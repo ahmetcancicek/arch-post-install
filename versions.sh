14e6f0c276ce83b50f8fcc324e95a09f7f3fb123
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-12-26 23:05:52 +0300

    Add new beta version of the script

---
+++ versions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# JetBrains Products
+export IntelliJIDEA_VERSION=2024.3.1.1
+export DataGrip_VERSION=2024.3.3
+export GoLand_VERSION=2024.2.1.1
+
+# Development Tools
+export GO_VERSION=1.23.1
+export MAVEN=3
+export MAVEN_VERSION=3.9.9
+export GRADLE_VERSION=8.12
+export SPRING_VERSION=3.3.3
+
+# Applications
+export POSTMAN_VERSION=11.23.3
+export ANKI_VERSION=24.06.3
+export DROIDCAM_VERSION=2.1.3
+export ZOOM_VERSION=6.2.5.2440
+export ZOTERO_VERSION=7.0.9 
